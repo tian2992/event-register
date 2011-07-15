44ae6b50eff06c3dc2609a8ed25543983cd30b9f
Sebastian Oliva <tian2992@gmail.com>
2011-07-15 12:22:09 -0600

    basic version

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+
+require 'register'
+run Sinatra::Application
+
---
+++ register.rb
@@ -0,0 +1,74 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb' # easier for beginners than haml...
+require 'dm-core'
+require 'dm-validations'
+require 'dm-timestamps'
+require 'dm-migrations'
+
+
+if ENV['DATABASE_URL'] #are we running on Heroku (or a properly configured host?)
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+else #so, running locally, eh?
+  DataMapper::Logger.new($stdout, :debug)  
+  DataMapper.setup(:default, 'sqlite:///tmp/register-test.db')
+end
+
+class Registree
+  include DataMapper::Resource
+
+  property :user_id,   Serial
+  property :name,      String, :length => 120
+  property :email,     String, :format => :email_address, :required => true,
+    :unique => true
+
+  def to_s
+    "Registrar #{user_id}, #{name}"
+  end
+
+end
+
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+#DataMapper.auto_migrate!
+
+#mainPage
+get '/' do
+  erb :index
+end
+
+get '/id/:num' do
+  @registree = Registree.first(:user_id => params[:num])
+  if @registree
+    erb :user_info
+  else
+    erb :no_record
+  end
+end
+
+get '/atendees' do
+  all_persons = Registree.all
+  return_list = ""
+  for person in all_persons do
+    return_list << "#{person.user_id},#{person.name},#{person.email}\n"
+  end
+  return return_list
+end
+
+get '/register' do
+  erb :register,:locals => { :title => 'lol' }
+end
+
+post '/register' do
+  registree = Registree.new(name=>params[:name], email=>params[:email])
+  if registree.save
+    redirect "/#{registree.user_id}"
+  else
+    erb :failed_record
+  end
+end
+
+get '/おはよ' do
+  return "尾羽よ　ございます！"
+end
